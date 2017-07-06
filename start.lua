be2eea99a5d82dfd66d13e833e5548103c809e7e
DOOBW <02.10.12.21@mail.ru>
2017-07-06 20:43:01 +0600

    Create start.lua

---
+++ start.lua
@@ -0,0 +1,25 @@
+local redstone = require('component').redstone
+local computer = require('computer')
+local event = require('event')
+local signal = computer.pullSignal
+redstone.setWakeThreshold(15)
+
+function computer.pullSignal(...)
+  local e = {signal(...)}
+  if e[1] == 'redstone_changed' and e[5] == 15 then
+    os.execute('reboot')
+  end
+  return table.unpack(e)
+end
+
+while true do
+  if _G.m_timer then
+    event.cancel(_G.m_timer)
+    _G.m_timer = nil
+  end
+  local a = {computer.users()}
+  for i = 1, #a do
+    computer.removeUser(a[i])
+  end
+  os.execute('terminal')
+end
