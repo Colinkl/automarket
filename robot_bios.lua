1fa38439c5478715a3040d9f2ca0d65afd8eec05
DOOBW <02.10.12.21@mail.ru>
2018-01-21 00:07:52 +0600

    Update robot_bios.lua

--- robot_bios.lua
+++ robot_bios.lua
@@ -1,25 +1,27 @@
 local modem = component.proxy(component.list('modem')())
 local robot = component.proxy(component.list('robot')())
 local address = '----'
-modem.open(12345)
-local status = 0
+modem.open(3215)
+local status = 'stop'
 local signal = computer.pullSignal
 
 function computer.pullSignal(...)
   local e = {signal(...)}
   if e[1] == 'modem_message' and e[3]:sub(1, 4) == address then
-    status = tonumber(e[6])
+    status = e[6]
   end
 end
 while true do
   computer.pullSignal(0.05)
-  if status == 1 then
+  if status == 'import' then
     if robot.suck(3) then
       robot.drop(0)
     end
-  elseif status == 2 then
+  elseif status == 'export' then
     if robot.suck(0) then
       robot.drop(3)
     end
+  elseif status == 'stop' then
+    computer.pullSignal(1)
   end
 end
